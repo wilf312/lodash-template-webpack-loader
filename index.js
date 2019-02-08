40303bf442ee8d32ac27a8080529828bd1537a76
OkadaGenya <g_okada@waku-2.com>
2019-02-08 21:27:37 +0900

    for webpack4

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ function lodashTemplateLoader(content, map) {
      *
      * Interpret configuration by combining query params and `lodashTemplateLoader` options
      */
-
-    var options = this.options.lodashTemplateLoader || {};
+    var options = {};
     if (_.isFunction(options)) {
         options = options.call(this, this) || {};
     }
